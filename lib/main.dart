fecf5d2a54ccc997da4c456acd45d17e3a936dd4
Eli <eli.axel.ang@gmail.com>
2022-08-21 11:29:57 +0800

    Rewrite2

--- lib/main.dart
+++
@@ -1,9 +0,0 @@
-import 'package:flutter/material.dart';
-
-import 'package:fabrics/app.dart';
-import 'package:flutter/rendering.dart';
-
-void main() {
-  //debugPaintSizeEnabled = true;
-  runApp(const App());
-}
--- lib/models/tests/question.dart
+++
@@ -1,6 +0,0 @@
-class Question<A, P> {
-  final A answer;
-  final P prompt;
-
-  const Question({required this.prompt, required this.answer});
-}
